561d1f4ceb768515407f6c2250a104c12c5c8f4b
Doug Turnbull <softwaredoug@gmail.com>
2014-07-23 16:40:15 -0400

    Add JSON explorer doohickey

--- webapp/frontend/app/scripts/app.js
+++ webapp/frontend/app/scripts/app.js
@@ -9,4 +9,4 @@
  * Main module of the application.
  */
 angular
-  .module('splain-app', ['LocalStorageModule', 'ui.bootstrap']);
+  .module('splain-app', ['LocalStorageModule', 'ui.bootstrap', 'gd.ui.jsonexplorer']);
