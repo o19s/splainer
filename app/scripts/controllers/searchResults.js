e0cc44a3d74e1382455b59f62603b1f0dec0ae75
Doug Turnbull <softwaredoug@gmail.com>
2015-12-08 12:27:11 -0500

    Upgrades splainer-search library to 1.3.2
    Upgrading splainer-search primarilly involved:
    - renaming our searchSvc, as it conflicts with the one in
      splainer-search (grr angular needs namespaces)
    - creating the searcher with slightly different options

--- app/scripts/controllers/searchResults.js
+++ app/scripts/controllers/searchResults.js
@@ -8,13 +8,13 @@
  * Controller of the frontendApp
  */
 angular.module('splain-app')
-  .controller('SearchResultsCtrl', function ($scope, searchSvc, settingsStoreSvc) {
-  
+  .controller('SearchResultsCtrl', function ($scope, spl_searchSvc, settingsStoreSvc) {
+
 
     $scope.search = {};
 
     /* Initiate a new search with the latest settings
-     * */ 
+     * */
     $scope.search.search = function() {
       var promise = Promise.create($scope.search.search);
       $scope.search.reset();
@@ -27,9 +27,9 @@ angular.module('splain-app')
 
     $scope.search.reset = function() {
       var searchSettings = settingsStoreSvc.settings;
-      $scope.currSearch = searchSvc.createSearch(searchSettings);
+      $scope.currSearch = spl_searchSvc.createSearch(searchSettings);
     };
-    
+
     $scope.search.reset();
 
   });
