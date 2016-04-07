8b8eac7c77304328e98de41eeb25296746da278a
Doug Turnbull <softwaredoug@gmail.com>
2016-04-06 21:24:19 -0400

    Fix settings toggling between search engines

--- app/scripts/controllers/settings.js
+++ app/scripts/controllers/settings.js
@@ -8,20 +8,26 @@ angular.module('splain-app')
     'esSettingsSvc',
     function ($scope, settingsStoreSvc, solrSettingsSvc, esSettingsSvc) {
 
-      $scope.whichEngine = settingsStoreSvc.settings.whichEngine;
-      $scope.searchSettings = settingsStoreSvc.settings[$scope.whichEngine];
+      $scope.settings = settingsStoreSvc.settings;
+      $scope.workingSettings = settingsStoreSvc.settings[settingsStoreSvc.settings.whichEngine];
+      $scope.workingWhichEngine = settingsStoreSvc.settings.whichEngine;
 
-      $scope.$watch('whichEngine', function() {
-        $scope.searchSettings = settingsStoreSvc.settings[$scope.whichEngine];
+      $scope.$watch('settings.whichEngine', function() {
+        $scope.workingWhichEngine = settingsStoreSvc.settings.whichEngine;
       });
 
+      $scope.$watch('workingWhichEngine', function() {
+        $scope.workingSettings = settingsStoreSvc.settings[$scope.workingWhichEngine];
+      });
+
+
       $scope.publishSearcher = function() {
-        if ( $scope.whichEngine === 'solr' ) {
+        if ( $scope.workingWhichEngine === 'solr' ) {
           settingsStoreSvc.settings.whichEngine = 'solr';
-          solrSettingsSvc.fromTweakedSettings($scope.searchSettings);
-        } else if ( $scope.whichEngine === 'es' ) {
+          solrSettingsSvc.fromTweakedSettings($scope.workingSettings);
+        } else if ( $scope.workingWhichEngine === 'es' ) {
           settingsStoreSvc.settings.whichEngine = 'es';
-          esSettingsSvc.fromTweakedSettings($scope.searchSettings);
+          esSettingsSvc.fromTweakedSettings($scope.workingSettings);
         }
 
         $scope.search.search()
