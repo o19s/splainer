cbd4f606e5cafeef4037e79691f7816f8e2f2da4
Youssef Chaker <youssef@bearandgiraffe.com>
2019-06-04 17:42:40 -0500

    Fixes errors from upgrading version.

--- app/scripts/controllers/docRow.js
+++ app/scripts/controllers/docRow.js
@@ -1,45 +1,48 @@
 'use strict';
 
 angular.module('splain-app')
-  .controller('DocRowCtrl', function DocRowCtrl($scope, $modal) {
+  .controller('DocRowCtrl', [
+    '$scope',
+    '$uibModal',
+    function DocRowCtrl($scope, $uibModal) {
 
-    $scope.docRow = {};
-    $scope.docRow.snippets = function(doc) {
-      var snippets = doc.subSnippets('<em>', '</em>');
-      angular.forEach(snippets, function(value, key){
-        if ( angular.isArray(value) ) {
-          snippets[key] = value.join('');
-        }
-      });
-      return snippets;
-    };
-
-    $scope.doc.showDetailed = function() {
-      $modal.open({
-        templateUrl: 'views/detailedExplain.html',
-        controller: 'DocExplainCtrl',
-        size: 'lg',
-        resolve: {
-          doc: function() {
-            return $scope.doc;
-          },
-          canExplainOther: function() {
-            return $scope.whichEngine === 'solr';
+      $scope.docRow = {};
+      $scope.docRow.snippets = function(doc) {
+        var snippets = doc.subSnippets('<em>', '</em>');
+        angular.forEach(snippets, function(value, key){
+          if ( angular.isArray(value) ) {
+            snippets[key] = value.join('');
           }
-        }
-      });
-    };
+        });
+        return snippets;
+      };
 
-    $scope.doc.showDoc = function() {
-      $modal.open({
-        templateUrl: 'views/detailedDoc.html',
-        controller: 'DetailedDocCtrl',
-        resolve: {
-          doc: function() {
-            return $scope.doc;
+      $scope.doc.showDetailed = function() {
+        $uibModal.open({
+          templateUrl: 'views/detailedExplain.html',
+          controller: 'DocExplainCtrl',
+          size: 'lg',
+          resolve: {
+            doc: function() {
+              return $scope.doc;
+            },
+            canExplainOther: function() {
+              return $scope.whichEngine === 'solr';
+            }
           }
-        }
-      });
-    };
+        });
+      };
 
-  });
+      $scope.doc.showDoc = function() {
+        $uibModal.open({
+          templateUrl: 'views/detailedDoc.html',
+          controller: 'DetailedDocCtrl',
+          resolve: {
+            doc: function() {
+              return $scope.doc;
+            }
+          }
+        });
+      };
+    }
+  ]);
