6ea3a1d09460e31e8797a62e1bbbfe248c5e0807
Daniel Worley <danworley@gmail.com>
2020-02-07 15:33:35 -0500

    Read current search engine from settings, not scope

--- app/scripts/controllers/docRow.js
+++ app/scripts/controllers/docRow.js
@@ -4,7 +4,8 @@ angular.module('splain-app')
   .controller('DocRowCtrl', [
     '$scope',
     '$uibModal',
-    function DocRowCtrl($scope, $uibModal) {
+    'settingsStoreSvc',
+    function DocRowCtrl($scope, $uibModal, settingsStoreSvc) {
 
       $scope.docRow = {};
       $scope.docRow.snippets = function(doc) {
@@ -28,7 +29,7 @@ angular.module('splain-app')
             },
             canExplainOther: function() {
               var allowedEngines = ['es', 'solr'];
-              return allowedEngines.includes($scope.whichEngine);
+              return allowedEngines.includes(settingsStoreSvc.settings.whichEngine);
             }
           }
         });
